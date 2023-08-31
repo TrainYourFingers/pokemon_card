006d994ba06b61779c8d05136aee7ac89a15fefd
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2023-08-31 17:25:56 +0545

    all data added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
-import { ImageProps, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import {
+  ImageProps,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 import PokemonCard from "./components/PokemonCard";
 import React from "react";
 
@@ -20,12 +26,41 @@ const App = () => {
     moves: ["Scratch", "Ember", "Growl", "Leer"],
     weakness: ["Water", "Rock"],
   };
+  const bulbasaurData: Pokemon = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"),
+    type: "Grass",
+    hp: 57,
+    moves: ["Tie", "Ember", "Growl", "Leer"],
+    weakness: ["Blade", "Fire"],
+  };
+  const pikachuData: Pokemon = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"),
+    type: "Electric",
+    hp: 43,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weakness: ["Water", "Rock"],
+  };
+  const squirtleData: Pokemon = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"),
+    type: "Water",
+    hp: 35,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weakness: ["Fire", "Soil"],
+  };
 
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
-      <PokemonCard {...charmanderData} />
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView style={styles.safeContainer}>
+        <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+        <PokemonCard {...squirtleData} />
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
