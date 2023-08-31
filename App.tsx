97ea3c8e8abe75ddfa7562a0e56efcac6b3e4f03
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2023-08-31 09:42:10 +0545

    props working

--- App.tsx
+++ App.tsx
@@ -1,22 +1,40 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import PokemonCard from "./components/PokemonCard";
 import React from "react";
 
-type Props = {};
+type Pokemon = {
+  name: string;
+  image: string;
+  type: string;
+  hp: number;
+  moves: string[];
+  weakness: string[];
+};
 
-const App = (props: Props) => {
+const App = () => {
+  const charmanderData: Pokemon = {
+    name: "Charmander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weakness: ["Water", "Rock"],
+  };
+
   return (
     <SafeAreaView style={styles.safeContainer}>
-      <PokemonCard />
+      <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
+      <PokemonCard {...charmanderData} />
     </SafeAreaView>
   );
 };
-
-export default App;
 
 const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
     backgroundColor: "#f5f5f5",
+    padding: 10,
   },
 });
+
+export default App;
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,16 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Image,
+  ImageProps,
+} from "react-native";
 import React from "react";
 
-type Props = {};
+type Pokemon = {
+  name: string;
+  image: ImageProps;
+  type: string;
+  hp: number;
+  moves: string[];
+  weakness: string[];
+};
 
-const PokemonCard = (props: Props) => {
+const PokemonCard = ({ name, image, type, hp, moves, weakness }: Pokemon) => {
   return (
-    <View>
-      <Text>PokemonCard</Text>
+    <View style={styles.card}>
+      <View>
+        <Text>{name}</Text>
+        <Text>{hp}</Text>
+      </View>
+      <Image source={image} accessibilityLabel={`${name} Pokemon`} />
+      <View>
+        <Text>{type}</Text>
+      </View>
+      <View>
+        <Text>Moves: {moves.join(", ")}</Text>
+      </View>
+      <View>
+        <Text>Weakness: {weakness.join(", ")}</Text>
+      </View>
     </View>
   );
 };
 
 export default PokemonCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
