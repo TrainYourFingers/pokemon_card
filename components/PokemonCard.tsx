f1b5cb7baaff581fdba5b764a45569b3a27f831f
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2023-08-31 09:17:59 +0545

    first commit

---
+++ components/PokemonCard.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const PokemonCard = (props: Props) => {
+  return (
+    <View>
+      <Text>PokemonCard</Text>
+    </View>
+  );
+};
+
+export default PokemonCard;
+
+const styles = StyleSheet.create({});
